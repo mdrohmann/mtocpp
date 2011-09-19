0c4889c8f3835f6e47e486eb10686ec59ea6df5a
Martin Drohmann <mdrohmann@gmail.com>
2011-09-19 17:05:53 +0200

    fix Bug #74

--- test/doxygen.m
+++ test/doxygen.m
@@ -74,7 +74,7 @@ function rv = doxygen(param1, param2, auto_param)
 % of argument respectively return argument descriptions.
 %
 % Parameters:
-%  param1: first parameter of type testnamespace.testclass
+%  param1: first parameter of type testnamespace.testclass.
 %
 % Return values:
 %  rv: return value
