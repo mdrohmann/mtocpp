288e57fb04ce2dbbea2f0ff40c33a17abf33698d
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2011-11-28 13:10:20 +0100

    Initial mtoc++ 1.3 commit

--- src/docs/changelist.c
+++ src/docs/changelist.c
@@ -5,12 +5,15 @@
  * Here are all the changes/new features sorted by versions of mtoc++:
  * - @subpage newfeat01  - @subpage changelog01
  * - @subpage newfeat12  - @subpage changelog12
+ * - @subpage newfeat13  - @subpage changelog13
  *
  * @attention The repeated occurence of the new features/changes in this specific site below is just due to the fact that
  * the mtoc++ features/changes themselves have to be written down somewhere. Under usual circumstances those tags below
  * would be placed inside the MatLab files/functions/classes where the actual change happened; see the comments
  * from the MatlabDocMaker as an example. So the list below is not necessarily complete, but the sites referenced above contain all
  * new features / changes!
+ *
+ * @new{1,3,dw,2011-11-28} Started mtoc++ 1.3.
  *
  * @change{1,2,md,2011-11-28}
  * Allow long (including line breaks) default values for properties
@@ -39,14 +42,25 @@
  * See also @ref newfeat01
  *
  * @page newfeat12 New features in mtoc++ 1.2
- * @short Initial release version with this feature.
+ * @short First "stable" release with windows/unix support.
  *
  * See also @ref changelog12
  *
  *
  * @page changelog12 Changes in mtoc++ 1.2
- * @short Initial release version with this feature.
+ * @short First "stable" release with windows/unix support.
  *
  * See also @ref newfeat12
+ *
+ * @page newfeat13 New features in mtoc++ 1.3
+ * @short Current development
+ *
+ * See also @ref changelog13
+ *
+ *
+ * @page changelog13 Changes in mtoc++ 1.3
+ * @short Current development
+ *
+ * See also @ref newfeat13
  *
  */
