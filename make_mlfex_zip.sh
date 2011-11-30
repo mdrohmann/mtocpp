0c92fe6b6a41c985386121640b5118abae7d3421
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2011-11-30 14:47:31 +0100

    excluded the binaries from the fileexchange version and added a download link for binaries only at the download page

--- make_mlfex_zip.sh
+++ make_mlfex_zip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 tmpdir=mtoc++_$1
 mkdir $tmpdir
-cp -r tools src cmake CMakeLists.txt INSTALL test win32 $tmpdir
+cp -r tools src cmake CMakeLists.txt INSTALL test $tmpdir
 zip -r mtoc++_mlfex_$1.zip $tmpdir
 rm -rf $tmpdir
+wbins=mtoc++_win32bins_$1.zip
+zip $wbins win32/*
+mv $wbins ~/aghwww/MoRePaS/software/mtocpp
