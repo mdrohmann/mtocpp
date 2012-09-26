6803806ec56e9c626d143d1871a647ebb51ad562
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-09-26 11:43:02 +0200

    small fixes in docs related to previous release links

--- make_mlfex_zip.sh
+++ make_mlfex_zip.sh
@@ -2,7 +2,7 @@
 echo "Creating MatLab File Exchange export version..."
 tmpdir=mtoc++_$1
 mkdir $tmpdir
-cp -r tools src cmake CMakeLists.txt License.txt INSTALL test $tmpdir
+cp -r tools src cmake CMakeLists.txt INSTALL test $tmpdir
 zip -r mtoc++_mlfex_$1.zip $tmpdir
 rm -rf $tmpdir
 # Also create the windows binaries only
